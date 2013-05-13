a1b40e2b05cf21d0f3ea5054c97033e5513f2d8b
Shane Yuan <ychensha@gmail.com>
2013-05-13 14:29:02 +0800

    MydownloadPage:UpdateListBox define, NewTaskModal: TaskSpace added

--- XAR/HelloBolt/layout/NewTaskModal.xml.lua
+++ XAR/HelloBolt/layout/NewTaskModal.xml.lua
@@ -8,16 +8,45 @@ function OnCancelClick(self)
 end
 
 function OnNewTaskItemCheck(self, eventName, index, isCheck)
-	--local owner = self:GetOwner()
-	--local spaceNeeded = owner:GetUIObject("taskspace.content")
 	local owner = self:GetOwner()
 	local hostwnd = owner:GetBindHostWnd()
+	local spaceNeeded = owner:GetUIObject("taskspace.content")
+	local listbox = owner:GetUIObject("listbox")
+	local listboxAttr = listbox:GetAttribute()
 	--XLMessageBox( self:GetItemByIndex(index + 1):GetControlObject("name"):GetText() )
 	local userData = hostwnd:GetUserData()
+	if userData.TotalSize == nil then
+		userData.TotalSize = 0
+	end
 	if userData.SelectedFiles == nil then
 		userData.SelectedFiles = {}
 	end
-	table.insert(userData.SelectedFiles, self:GetItemByIndex(index + 1):GetControlObject("name"):GetText())
+	if isCheck then
+		table.insert(userData.SelectedFiles, self:GetItemByIndex(index):GetControlObject("name"):GetText())
+	else
+		
+	end
+	if isCheck then
+		userData.TotalSize = userData.TotalSize + listboxAttr.ItemDataTable[index].Size
+	else
+		userData.TotalSize = userData.TotalSize - listboxAttr.ItemDataTable[index].Size
+	end
+	if spaceNeeded ~= nil then
+		local KB = 1024
+		local MB = KB*1024
+		local GB = MB*1024
+		local content
+		if userData.TotalSize > GB then
+			content = string.format("%.2f GB", userData.TotalSize/GB)
+		elseif userData.TotalSize > MB then
+			content = string.format("%.2f MB", userData.TotalSize/MB)
+		elseif userData.TotalSize > KB then
+			content = string.format("%.2f KB", userData.TotalSize/KB)
+		else
+			content = string.format("%d B", userData.TotalSize)
+		end
+	    spaceNeeded:SetText(content)
+	end
 end
 
 function OnOKClick(self)
